dc3928d80a4f035919730759555e1c38df1cc5a9
michael111999 <micsasson7@gmail.com>
2025-11-14 12:39:58 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,15 +1,35 @@
 def split_before_each_uppercases(formula):
-    x=""
-    y=""
-    lst[x, y]
-    for i in formula:
-        if i>=a and i<=z:
-            x+=i
-        elif i>=0:
-            y+=i
-    return lst
+    x = ""
+    y = ""
+    uppercase_found = False
+    
+    for ch in formula:
+        if not uppercase_found and ch.isupper():
+            uppercase_found = True
+            y += ch
+        elif uppercase_found:
+            y += ch
+        else:
+            x += ch
+
+    return x, y
+    
+   
             
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    x = ""
+    y = ""
+    digit_found = False
+    
+    for ch in formula:
+        if not digit_found and ch.isdigit():
+            digit_found = True
+            y += ch
+        elif digit_found:
+            y += ch
+        else:
+            x += ch
+
+    return x, y
