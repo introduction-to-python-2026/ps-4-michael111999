ae0f762cfdd10268b5711110c1225c56f49eb86c
michael111999 <micsasson7@gmail.com>
2025-11-14 17:47:36 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,18 +1,18 @@
 def split_before_each_uppercases(formula):
-    x = ""
-    y = ""
-    
+    x=[]
+    n=0
     
     for ch in formula:
         if ch.isupper():
+            x.append(formula[n:ch]
+            n==len(formula[n:ch])
             
            
         
-            y += ch
-        elif ch.isalpha():
-            x += ch
+            
+       
 
-    return x, y
+    return x
     
    
             
