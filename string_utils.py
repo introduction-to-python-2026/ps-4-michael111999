ea36008b555a9e489245efb6f9c76e3da236555f
michael111999 <micsasson7@gmail.com>
2025-11-14 12:50:20 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,15 +1,15 @@
 def split_before_each_uppercases(formula):
     x = ""
     y = ""
-    uppercase_found = False
+    
     
     for ch in formula:
-        if not uppercase_found and ch.isupper():
-            uppercase_found = True
+        if ch.isupper():
+            
+           
+        
             y += ch
-        elif uppercase_found:
-            y += ch
-        else:
+        elif ch.isalpha():
             x += ch
 
     return x, y
@@ -21,15 +21,15 @@ def split_before_each_uppercases(formula):
 def split_at_first_digit(formula):
     x = ""
     y = ""
-    digit_found = False
+    
     
     for ch in formula:
-        if not digit_found and ch.isdigit():
-            digit_found = True
+        if ch.isdigit():
+            
             y += ch
-        elif digit_found:
-            y += ch
-        else:
+       
+            
+        elif ch.isalpha():
             x += ch
 
     return x, y
